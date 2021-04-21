ffc25a91d86a70f593fd63b2f33cd5d33d1f5095
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-22 01:31:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = "1661403617:AAGe_QJL9dIx2_-xXgfT_2TJHsnLHPEqp2I"
-API_ID = 69
-API_HASH = "e46b6c854d2bf580"
+API_ID = 4082595
+API_HASH = "a6cd949d783c11fa52fd145bf1b933f6"
 PHONE_NUMBER = "+910000000000" # Need for Helper Userbot
 MAIN_CHATS = [-1001191798578] # NOTE These are the chats where main functions of bot will work, like music downloading etc.
 SUDO_USERS_ID = [1763082886,1731486754] # Sudo users have full access to everythin, don't trust anyone
