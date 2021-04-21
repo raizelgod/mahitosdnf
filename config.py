c8f425f3cde37ec1e0c1ed054550d9b19e1cc21e
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-22 01:30:29 +0530

    Update and rename sample_config.py to config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+BOT_TOKEN = "1661403617:AAGe_QJL9dIx2_-xXgfT_2TJHsnLHPEqp2I"
+API_ID = 69
+API_HASH = "e46b6c854d2bf580"
+PHONE_NUMBER = "+910000000000" # Need for Helper Userbot
+MAIN_CHATS = [-1001191798578] # NOTE These are the chats where main functions of bot will work, like music downloading etc.
+SUDO_USERS_ID = [1763082886,1731486754] # Sudo users have full access to everythin, don't trust anyone
+LOG_GROUP_ID = -1001160567813
+GBAN_LOG_GROUP_ID = -1001436725405
+FERNET_ENCRYPTION_KEY = "iKMq0WZMnJKjMQxZWKtv-cplMuF_LoyshXj0XbTGGWM="
+WELCOME_DELAY_KICK_SEC = 300
+MONGO_DB_URI = "mongodb+srv://hellxgodlike:gabriel25@cluster0.cti6j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+# NOTE Don't make changes below this line
+ARQ_API_BASE_URL = "http://35.240.133.234:8000"
